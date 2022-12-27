04b2ee80758660dcce6523e01405bcac65e1ae84
limingqihust <1285057590@qq.com>
2022-12-27 15:45:10 +0800

    my work on lab1_challenge1_backtrace is done

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -36,56 +36,26 @@ ssize_t sys_user_print_backtrace(trapframe* tf,int backtrace_num) {
   elf_ctx elfloader;
   elf_info info;
   info.f = spike_file_open("obj/app_print_backtrace", O_RDONLY, 0);
-  // info.p = current;
   /*读取elf header到elfloader.ehdr中*/
   if(elf_init(&elfloader, &info)!=EL_OK)
     panic("elf init error\n");
-  elf_sect_header sh_addr;
-  int i;
-  int off;/*当前读取的section header的地址*/
-  for(i=0,off=elfloader.ehdr.shoff;i<elfloader.ehdr.shnum;i++,off+=sizeof(sh_addr))
+  elf_sect_header section_header[elfloader.ehdr.shnum];
+  int i,off;
+  for(i=0,off=elfloader.ehdr.shoff;i<elfloader.ehdr.shnum;i++,off+=sizeof(elf_sect_header))
   {
     /*读取 section header*/
-    if(elf_fpread(&elfloader,(void*)&sh_addr,sizeof(sh_addr),off)!=sizeof(sh_addr))
+    if(elf_fpread(&elfloader,(void*)(section_header+i),sizeof(elf_sect_header),off)!=sizeof(elf_sect_header))
       panic("load section header into elfloader fail\n");
-    continue;
   }
 
-
   uint64 num_section_header=(uint64)elfloader.ehdr.shnum;//section header的数量
-  uint64 address_the_first_section_header=(uint64)elfloader.ehdr.shoff;//第1个section header的地址
+  uint64 first_section_header_off=(uint64)elfloader.ehdr.shoff;//第1个section header的地址
   uint64 size_section_header=(uint64)elfloader.ehdr.shentsize;//section header的大小
-  uint64 index_string_table=(uint64)elfloader.ehdr.shstrndx;//string table的索引值
+  uint64 shstrndx=elfloader.ehdr.shstrndx;//shstrndx的索引值
 
-
-
-  //section header symtab的地址 symtab存储函数变量名
-  uint64 section_header_symtab_address=address_the_first_section_header+15*size_section_header;
-
-  //section header strtab的地址
-  uint64 section_header_strtab_address=address_the_first_section_header+16*size_section_header;
-
-  //section header shstrtab的地址
-  uint64 section_header_shstrtab_address=address_the_first_section_header+17*size_section_header;
-
-  elf_sect_header symtab_header;
-  elf_sect_header strtab_header;
+  /*section header shstrtab的地址*/
+  uint64 section_header_shstrtab_address=first_section_header_off+shstrndx*size_section_header;
   elf_sect_header shstrtab_header;
-  /*读取symtab*/
-  if(elf_fpread(&elfloader,(void*)&symtab_header,sizeof(symtab_header),section_header_symtab_address)!=sizeof(symtab_header))
-    panic("load section header fail");
-  int num_symbol=symtab_header.sh_size/sizeof(Elf64_Sym);
-  Elf64_Sym symbol[num_symbol];
-  if(elf_fpread(&elfloader,(void*)&symbol,symtab_header.sh_size,symtab_header.sh_offset)!=symtab_header.sh_size)
-    panic("load section symtab fail");
-  
-  
-  /*读取strtab*/
-  if(elf_fpread(&elfloader,(void*)&strtab_header,sizeof(strtab_header),section_header_strtab_address)!=sizeof(strtab_header))
-    panic("load section header fail");
-  char strtab[strtab_header.sh_size];
-  if(elf_fpread(&elfloader,(void*)&strtab,strtab_header.sh_size,strtab_header.sh_offset)!=strtab_header.sh_size)
-    panic("load section strtab fail");
 
   /*读取shstrtab*/
   if(elf_fpread(&elfloader,(void*)&shstrtab_header,sizeof(shstrtab_header),section_header_shstrtab_address)!=sizeof(shstrtab_header))
@@ -93,6 +63,33 @@ ssize_t sys_user_print_backtrace(trapframe* tf,int backtrace_num) {
   char shstrtab[shstrtab_header.sh_size];
   if(elf_fpread(&elfloader,(void*)&shstrtab,shstrtab_header.sh_size,shstrtab_header.sh_offset)!=shstrtab_header.sh_size)
     panic("load section shstrtab fail");
+  int idx_symtab=0,idx_strtab=0;
+  for(int i=0;i<elfloader.ehdr.shnum;i++)
+  {
+    if(strcmp(shstrtab+section_header[i].sh_name,".symtab")==0)
+    {
+      idx_symtab=i;
+      break;
+    }
+  }
+  for(int i=0;i<elfloader.ehdr.shnum;i++)
+  {
+    if(strcmp(shstrtab+section_header[i].sh_name,".strtab")==0)
+    {
+      idx_strtab=i;
+      break;
+    }
+  }  
+  /*读取symtab*/
+  int num_symbol=section_header[idx_symtab].sh_size/sizeof(Elf64_Sym);
+  Elf64_Sym symbol[num_symbol];
+  if(elf_fpread(&elfloader,(void*)&symbol,section_header[idx_symtab].sh_size,section_header[idx_symtab].sh_offset)!=section_header[idx_symtab].sh_size)
+    panic("load section symtab fail");
+
+  /*读取strtab*/
+  char strtab[section_header[idx_strtab].sh_size];
+  if(elf_fpread(&elfloader,(void*)&strtab,section_header[idx_strtab].sh_size,section_header[idx_strtab].sh_offset)!=section_header[idx_strtab].sh_size)
+    panic("load section strtab fail");
   spike_file_close( info.f );
 
   uint64 ra=tf->regs.ra;
@@ -117,7 +114,25 @@ ssize_t sys_user_print_backtrace(trapframe* tf,int backtrace_num) {
       }
     }
   }
-  
+
+
+
+  ra=tf->regs.ra;
+  sp=tf->regs.sp;
+  s0=tf->regs.s0;
+  sprint("ra=%llx sp=%llx s0=%llx\n",ra,sp,s0);
+  ra=*(uint64*)(sp+8);//函数的返回地址
+  s0=*(uint64*)(sp);//
+  sp+=32;
+  for(int i=0;i<backtrace_num;i++)
+  {
+    ra=*(uint64*)(sp+8);
+    s0=*(uint64*)(sp);
+    sp+=16;
+    sprint("func_addr=%llx\n",ra);
+  }
+
+
   return 0;
 }
 //
